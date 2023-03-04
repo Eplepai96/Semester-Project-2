8a07d07470b235ef703697380c5b6c42ab9a7213
Eplepai96 <bjorn.andreas1996@gmail.com>
2023-03-04 03:13:45 +0100

    adds router

---
+++ src/js/router.mjs
@@ -0,0 +1,39 @@
+import * as listeners from "./handlers/index.mjs";
+
+
+
+export default function router() {
+  const path = location.pathname;
+  
+  switch (path) {
+    case '/index.html':
+      listeners.setLogoutListener()
+      listeners.setDisplayListingsListener()
+      return;
+    case '/listings/':
+      listeners.setDisplayListingsListener()
+      return;
+    case '/listings/listing/':
+      listeners.setDisplayListingListener()
+      listeners.setBidListener()
+      return;
+    case '/profile/login/':
+      listeners.setLoginFormListener();
+      return;
+    case '/profile/register/':
+      listeners.setRegisterFormListener()
+      return;
+    case '/listing/create/':
+      listeners.setCreatePostFormListener()
+      return;
+    case '/post/edit/':
+      listeners.setUpdatePostListener()
+      return;
+    case '/profile':
+      listeners.setUpdateProfileListener()
+      return;
+    case '/profile/edit/':
+      listeners.setUpdateProfileListener()
+      return;
+  }
+}
